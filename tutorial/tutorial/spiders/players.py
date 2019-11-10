5bfd0028f62ef5517134fb563308b2effaefc4d5
Susana Benavidez <sbenavid@stanford.edu>
2019-11-09 18:46:44 -0800

    removing tutorial folder

--- tutorial/tutorial/spiders/players.py
+++
@@ -1,9 +0,0 @@
-import scrapy
-from scrapy.crawler import CrawlerProcess
-
-class PlayerStats(scrapy.Spider):
-    name = "PlayerStats"
-    start_urls = ['https://www.baseball-reference.com/players/b/bernato01.shtml', 'https://www.baseball-reference.com/players/m/mariomi01.shtml']
-    def parse(self, response):
-        table = response.xpath('//*[@class="row_summable sortable stats_table"]')
-        return table
